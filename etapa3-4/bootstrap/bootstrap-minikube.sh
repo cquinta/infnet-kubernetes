b1913d5bac28149399816f2eeca11656cd6ed769
Carlos Quintanilha <cquinta@gmail.com>
2024-10-17 20:17:28 -0300

    ajustes

--- etapa3-4/bootstrap/bootstrap-minikube.sh
+++ etapa3-4/bootstrap/bootstrap-minikube.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+#Update and install curl and wget
 apt-get update -y 
 apt-get install -y curl wget
+
+#Install Docker
 curl -fsSL https://get.docker.com | bash
 
+#Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#Install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
